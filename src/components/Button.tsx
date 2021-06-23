c0ddcc65f7e95774d5429cccbb64739d57aeec24
Matheus Lucas <matheuslucas.ssilva@gmail.com>
2021-06-22 22:53:09 -0300

    Criar componente Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,7 @@
+import { ButtonHTMLAttributes } from "react";
+
+import "../styles/button.scss";
+
+export default function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
+  return <button className="button" {...props}></button>;
+}
